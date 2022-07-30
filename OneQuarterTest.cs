50be9fe2c1dc867854ab9695c636ab9e672f6903
Anastasiya Ya <gelvy@yandex.ru>
2022-07-30 16:06:27 +0300

    Created a method to solve the task

--- OneQuarterTest.cs
+++ OneQuarterTest.cs
@@ -1,3 +1,20 @@
 ﻿// Написать программу, которая из имебщегося массива строк формирует массив из строк, длина которых меньше или равна 3 символа.
 
 string[] MyArray = new string[] {"hello", "2", "world", ":-)"};
+
+string[] SelectThreeSymbolElements(string[] array, int m)
+{
+    string[] newArray = new string[array.Length];
+
+    int j = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= m) 
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
